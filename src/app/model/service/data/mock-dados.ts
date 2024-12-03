c54d65e0d7ac64ffe4d7e43713c41b90c86e3a66
fifizinho <132359775+fifizinho@users.noreply.github.com>
2024-12-03 16:30:48 -0300

    BlogSenaiFE

---
+++ src/app/model/service/data/mock-dados.ts
@@ -0,0 +1,29 @@
+export const posts = [
+    {
+        id: 1,
+        title: 'Exemplo do Post',
+        author: 'Admin',
+        date: new Date(),
+        content: 1,
+        description: 'Esse é um post de exemplo.',
+        imageUrl: "./rep1.png"
+    },
+    {
+        id: 2,
+        title: 'Como criar um Blog',
+        author: 'Editor',
+        date: new Date(),
+        content: 1,
+        description: 'Guia para criar o proprio blog.',
+        imageUrl: "./rep2.png"
+    },
+    {
+        id: 3,
+        title: 'How successfull people make the most of their business',
+        author: 'Amco',
+        date: new Date(),
+        content: 1,
+        description: 'Quality Care For Every Patient Contraty to popular belief, Lorem Ipsum.',
+        imageUrl: "./rep3.png"
+    },
+]
--- src/app/nav-bar/nav-bar.component.spec.ts
+++ src/app/nav-bar/nav-bar.component.spec.ts
@@ -1,18 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { CardsComponent } from './cards.component';
+import { NavBarComponent } from './nav-bar.component';
 
-describe('CardsComponent', () => {
-  let component: CardsComponent;
-  let fixture: ComponentFixture<CardsComponent>;
+describe('NavBarComponent', () => {
+  let component: NavBarComponent;
+  let fixture: ComponentFixture<NavBarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CardsComponent]
+      imports: [NavBarComponent]
     })
     .compileComponents();
 
-    fixture = TestBed.createComponent(CardsComponent);
+    fixture = TestBed.createComponent(NavBarComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
